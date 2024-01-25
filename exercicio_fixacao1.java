8df1c1676bc04bed1ddc17564df4a1f213154e51
lucasmissura <lucas.missura29@gmail.com>
2024-01-25 00:24:19 -0300

    Create exercicio_fixacao1.java
    Exercício realizado para prática e aperfeiçoamento das variáveis da linguagem Java

---
+++ exercicio_fixacao1.java
@@ -0,0 +1,31 @@
+package Exercicio_fixacao1;
+
+public class exercicio_fixacao1 {
+
+	public static void main(String[] args) {
+		
+		String product1 = "Computer";
+		String product2 = "Office Desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, wich price is $ %.2f%n", product1, price1);
+		System.out.printf("%s, wich price is $ %.2f%n", product2, price2);
+		System.out.println();
+		System.out.printf("Record: %d years old, code %d and gender %s", age, code, gender);
+		System.out.println();
+		System.out.printf("Measue with eight decima places: %f%n", measure);
+		System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+		
+		
+
+	}
+
+}
