0e4f592acf8ab5d1b3f134baedb586674b989c3c
lucasmissura <lucas.missura29@gmail.com>
2024-01-25 16:05:21 -0300

    Exercício de fixação 5
    Neste projeto foi utilizado a estrutura repetitiva "FOR" para executar um código que, ao colocar um número,  de a sequência de 1 até "N" com a multiplicação i/ i*i/  i*i*i.

---
+++ Exercicio_fixacao5.java
@@ -0,0 +1,24 @@
+package exercicio_fixacao5;
+
+import java.util.Scanner;
+
+public class Exercicio_fixacao5 {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+		}
+		
+		sc.close();
+	}
+
+}
