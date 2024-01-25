e626ac6354da1cb38ed654380a3a758a30db44e1
lucasmissura <lucas.missura29@gmail.com>
2024-01-25 00:25:58 -0300

    Calculo de area
    Neste projeto foi utilizado um código que fosse capaz de calcular a área com medidas já definidas

---
+++ teste_processamento.java
@@ -0,0 +1,26 @@
+package teste_processamento;
+
+public class teste_processamento {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		x = 5;
+		y = x * 2;
+		
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h= 5.0;
+		area = (b + B) / 2.0 * h;
+		
+		System.out.println("Exemplo 1");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println();
+		System.out.println("Exemplo 2");
+		System.out.println(area);
+
+	}
+
+}
